09a8e6ab4238becdb451a2fc26a9c02c0c65e186
MsParvika <43131186+MsParvika@users.noreply.github.com>
2019-03-07 17:29:32 -0700

    Update helpers.py

--- api/helpers.py
+++ api/helpers.py
@@ -1,5 +1,18 @@
 import glob
 import xml.etree.ElementTree as ET
+import pickle;
+
+
+
+def save(variable, flieName):
+    with open(flieName, 'wb') as f:
+        pickle.dump(variable, f);
+
+
+def load(flieName):
+    with open(flieName, 'rb') as f:
+        b = pickle.load(f)
+        return b;
 
 def sort(arr, new_object, max_length, sort_key, bit):
 
