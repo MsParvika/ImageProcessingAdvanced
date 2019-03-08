48f6d21381e7dab8adebbbb45c4bba68e3f467fc
MsParvika <43131186+MsParvika@users.noreply.github.com>
2019-03-07 17:51:45 -0700

    Update Task3.py

--- api2/Task3.py
+++ api2/Task3.py
@@ -1,47 +1,35 @@
-import time;
-from src import Helper as hp;
-from src import SpareMatrixPR as SMPR;
-from src import ShowImages as si;
+import helpers as hp;
+import SpareMatrixPR as SMPR;
+import ShowImages as si;
 import configparser as cp
 import os
-
-
 
 config = cp.ConfigParser()
 config.read(os.getcwd()+'/config.ini')
 
-k =input("Enter no. dominant images :  ");
-
-tic = time.clock();
+k =input("Enter no. dominant images :  ")
 
 # adjacency Matrix saved as part of task 1
 adj_matrix = hp.load("adj_matrix.txt")
 
 # imageId to Index
-IdsIdxMap = hp.load("IdsIdxMap.txt");
+IdsIdxMap = hp.load("IdsIdxMap.txt")
 
 # Index to Image Id
-IdxIdsMap = hp.load("IdxIdsMap.txt");
+IdxIdsMap = hp.load("IdxIdsMap.txt")
 
 
-pr2=SMPR.pagerank_scipy(adj_matrix, alpha=0.85, tol=1.0e-10);
+pr2=SMPR.pagerank_scipy(adj_matrix, alpha=0.85, tol=1.0e-10)
 
-imageidx =  pr2.argsort()[-int(k):][::-1];
+imageidx =  pr2.argsort()[-int(k):][::-1]
 
-print(pr2);
-print(len(pr2));
-print(imageidx);
-print(pr2[imageidx]);
-
-imageidxList = [];
+imageidxList = []
 
 for idx in imageidx:
-    imageidxList.append(IdxIdsMap[idx]);
+    imageidxList.append(IdxIdsMap[idx])
 
 
 devset_path = config['TASK3']['devset_path']
-toc = time.clock();
-print(toc-tic);
-si.fetchImages(imageidxList, devset_path);
+si.fetchImages(imageidxList, devset_path)
 
 
